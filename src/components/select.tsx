82831abead08762e7f23b9d1fdf3d626d72bc8a8
Theaveas So <theaveasso@Theaveass-MacBook-Pro.local>
2022-09-09 13:24:57 +0700

    finish select mui component

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,5 +1,46 @@
+import { Box, MenuItem, Stack, TextField } from '@mui/material';
+import React, { useState } from 'react';
+
 const MuiSelect = () => {
-	return <div>select</div>;
+	const [countries, setCountries] = useState<string[]>([]);
+	const handleSelCountry = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const value = event.target.value;
+		setCountries(typeof value === 'string' ? value.split(',') : value);
+	};
+	console.log(countries);
+
+	return (
+		<Box
+			style={{
+				width: '100vw',
+				height: '100vh',
+				alignItems: 'center',
+				display: 'flex',
+				justifyContent: 'center',
+				textAlign: 'center',
+			}}
+		>
+			<Box width='250px'>
+				<TextField
+					fullWidth
+					label='Select country'
+					value={countries}
+					onChange={handleSelCountry}
+					select
+					SelectProps={{
+						multiple: true,
+					}}
+					size='small'
+					color='info'
+					helperText='Select your country'
+				>
+					<MenuItem value='IND'>India</MenuItem>
+					<MenuItem value='USA'>USA</MenuItem>
+					<MenuItem value='AUS'>Australia</MenuItem>
+				</TextField>
+			</Box>
+		</Box>
+	);
 };
 
 export default MuiSelect;
